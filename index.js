02983a0912e52f9c6f0ee94564413814df2e88e6
Daniel Guzman <guzdan@live.com>
2021-10-06 20:31:14 -0500

    commit tree 5afec703af7caa8f05300461ca97392112e7d5f2

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  
+  if (distance > 2500){
+    result = "No can do."
+  } 
+  else if (distance <= 400){
+    result = "This one is on me!"
+  }
+  else if (distance > 2000){
+    result = "I will gladly take your thirty bucks."
+  }
+
+  return result;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = (city === "NYC") ? ("Ok, sounds good.") : ("No go.")
+  
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  let result;
+
+  switch(generosity){
+    case "generous" : 
+      result = "Thank you so much.";
+      break;
+    case "not as generous" :
+      result = "Thank you."
+      break;
+    case "thanks for everything" :
+      result = "Bye." 
+      break;
+  }
+  return result;
 }
+
